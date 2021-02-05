c77f6246f912899fce01d5048fd2b39a5debdb87
Hallis - home main <Hallis1221_main@users.noreply.github.com>
2021-02-05 22:36:44 +0100

    :x: Fail to add FCM

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
@@ -7,6 +11,7 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:loading_animations/loading_animations.dart';
 
 FirebaseAnalytics analytics;
+bool localDevMode = true;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,7 +37,6 @@ class MyApp extends StatelessWidget {
             FlutterError.onError =
                 FirebaseCrashlytics.instance.recordFlutterError;
             FirebaseCrashlytics.instance.checkForUnsentReports();
-            print(FlutterError.onError);
             analyticsTrace.stop();
           } catch (e) {
             throw FlutterError(
@@ -49,7 +53,7 @@ class MyApp extends StatelessWidget {
             ],
             routes: {
               '/': (context) {
-                return Text("Inital route");
+                return SharedScaffold(body: MessageHandler());
               },
             },
           );
@@ -60,15 +64,33 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class SharedScaffold extends StatelessWidget {
+class SharedScaffold extends StatefulWidget {
   final Widget body;
 
   SharedScaffold({@required this.body});
 
   @override
+  _SharedScaffoldState createState() => _SharedScaffoldState();
+}
+
+class _SharedScaffoldState extends State<SharedScaffold> {
+  @override
+  void initState() {
+    if (localDevMode) {
+      String host = Platform.isAndroid ? '10.0.2.2:8080' : 'localhost:8080';
+      FirebaseFirestore.instance.settings = Settings(
+        host: host,
+        sslEnabled: false,
+        persistenceEnabled: false,
+      );
+    }
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: this.body,
+      body: this.widget.body,
     );
   }
 }
@@ -77,5 +99,39 @@ class ErrorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SharedScaffold(body: Text("Something went wrong..."));
+  }
+}
+
+class MessageHandler extends StatefulWidget {
+  @override
+  _MessageHandlerState createState() => _MessageHandlerState();
+}
+
+class _MessageHandlerState extends State<MessageHandler> {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final FirebaseMessaging _fcm = FirebaseMessaging.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    print("!");
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: Text('FCM Push Notifications'),
+      ),
+    );
   }
 }
