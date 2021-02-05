355b35e5864244d203dc26b37bc6ff60372cabdd
Hallis - home main <Hallis1221_main@users.noreply.github.com>
2021-02-05 17:35:55 +0100

    :chart_with_upwards_trend: Add firebase navigation observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,33 @@
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+FirebaseAnalytics analytics;
 
 void main() {
+  analytics = FirebaseAnalytics();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Text("flutter demo"),
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(
+          analytics: analytics,
+        ),
+      ],
+      routes: {
+        '/': (context) => Text("Inital route"),
+      },
     );
   }
 }
