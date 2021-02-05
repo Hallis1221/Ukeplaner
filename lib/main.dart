5989660806caa116c4256893699d1423882b706f
Hallis - home main <Hallis1221_main@users.noreply.github.com>
2021-02-05 18:24:02 +0100

    :chart_with_upwards_trend: Firebase Crashlytics implementatioj

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,60 @@
 import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_performance/firebase_performance.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:loading_animations/loading_animations.dart';
 
 FirebaseAnalytics analytics;
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Trace analyticsTrace =
-      FirebasePerformance.instance.newTrace("analytics_trace");
-  Trace appRuntimeTrace =
-      FirebasePerformance.instance.newTrace("appRunTime_trace");
-  appRuntimeTrace.start();
-
-  analyticsTrace.start();
-  analytics = FirebaseAnalytics();
-  analyticsTrace.stop();
 
   runApp(MyApp());
-  appRuntimeTrace.stop();
 }
 
-class MyApp extends StatefulWidget {
-  // This widget is the root of your application.
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      navigatorObservers: [
-        FirebaseAnalyticsObserver(
-          analytics: analytics,
-        ),
-      ],
-      routes: {
-        '/': (context) => Text("Inital route"),
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return ErrorPage();
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          try {
+            Trace analyticsTrace =
+                FirebasePerformance.instance.newTrace("firebase_startup_trace");
+            analyticsTrace.start();
+            analytics = FirebaseAnalytics();
+            FlutterError.onError =
+                FirebaseCrashlytics.instance.recordFlutterError;
+            FirebaseCrashlytics.instance.checkForUnsentReports();
+            print(FlutterError.onError);
+            analyticsTrace.stop();
+          } catch (e) {
+            throw FlutterError(
+              "Failed to start firebase. Error: $e",
+            );
+          }
+          return MaterialApp(
+            title: 'Flutter Demo',
+            debugShowCheckedModeBanner: false,
+            navigatorObservers: [
+              FirebaseAnalyticsObserver(
+                analytics: analytics,
+              ),
+            ],
+            routes: {
+              '/': (context) {
+                return Text("Inital route");
+              },
+            },
+          );
+        }
+        return LoadingFlipping.circle();
       },
     );
   }
@@ -55,5 +70,12 @@ class SharedScaffold extends StatelessWidget {
     return Scaffold(
       body: this.body,
     );
+  }
+}
+
+class ErrorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SharedScaffold(body: Text("Something went wrong..."));
   }
 }
