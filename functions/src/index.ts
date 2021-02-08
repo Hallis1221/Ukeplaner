539b4faaceb801555cb4f084cda9726e1b456f67
Hallis school <Hallis1221@users.noreply.github.com>
2021-02-08 10:55:20 +0100

    revert

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,19 @@
 import * as functions from "firebase-functions";
+import * as firebase from 'https://www.gstatic.com/firebasejs/8.2.5/firebase-app.js';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+importScripts('https://www.gstatic.com/firebasejs/8.2.5/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.5/firebase-messaging.js');
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA3u8rA6k3AC7QTNZqI7UtVZ2XUvhvsx6k",
+    authDomain: "kg-ukeplaner.firebaseapp.com",
+    projectId: "kg-ukeplaner",
+    storageBucket: "kg-ukeplaner.appspot.com",
+    messagingSenderId: "113842149906",
+    appId: "1:113842149906:web:ccf591d45acf69226d96cb",
+    measurementId: "G-XMYW1L2P8Z"
+  };
+
+  
