12aa11e6b86371f18cd0bf612b0d5635aadba0c4
Hallis - home main <halvorvivelstad@gmail.com>
2021-02-23 19:07:54 +0100

    Update verify.dart

--- lib/screens/verify.dart
+++ lib/screens/verify.dart
@@ -6,8 +6,63 @@ class VerifyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Theme.of(context).backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Center(
+                  child: Card(
+                    elevation: 2,
+                    child: ClipPath(
+                      child: Container(
+                        height: 100,
+                        width: 100,
+                      ),
+                      clipper: ShapeBorderClipper(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          BottomCircle(),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomCircle extends StatelessWidget {
+  const BottomCircle({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Theme.of(context).primaryColor,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            blurRadius: 10,
+            color: Theme.of(context).shadowColor,
+          )
+        ],
+        borderRadius: BorderRadius.vertical(
+          top: Radius.elliptical(
+            MediaQuery.of(context).size.width * 3,
+            250,
+          ),
+        ),
       ),
     );
   }
