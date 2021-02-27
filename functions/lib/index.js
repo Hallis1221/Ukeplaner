b26047db1f8bde451296006d8bf5da9d78728f0b
Hallis - home main <halvorvivelstad@gmail.com>
2021-02-27 23:27:00 +0100

    Update index.js

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -3,8 +3,9 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 admin.initializeApp();
 const db = admin.firestore();
-exports.checkcode = functions.https.onCall((code) => {
+exports.checkcode = functions.https.onCall((argumentData) => {
     var valid = false;
+    var type;
     const docRef = db.collection('codes').doc('joinCodes');
     const result = docRef.get().then((doc) => {
         function exists() {
@@ -13,13 +14,15 @@ exports.checkcode = functions.https.onCall((code) => {
                 return null;
             }
             var data = doc.data()["studentCodes"];
-            var codeData = data[code["code"]];
+            var codeData = data[argumentData["code"]];
             if (codeData != undefined) {
+                type = "student";
                 return codeData;
             }
             data = doc.data()["teacherCodes"];
-            codeData = data[code["code"]];
+            codeData = data[argumentData["code"]];
             if (codeData != undefined) {
+                type = "teacher";
                 return codeData;
             }
             return null;
@@ -31,8 +34,28 @@ exports.checkcode = functions.https.onCall((code) => {
         else {
             valid = false;
         }
+        console.log(argumentData["useLog"] == true);
+        if (argumentData["useLog"] == true && valid) {
+            var key = argumentData["code"];
+            var codesList = {};
+            var list = { "used": instance["used"] + 1, "maxUses": 3, "valid": true };
+            if (type == "student") {
+                console.log(type);
+                codesList["studentCodes"] = {};
+                codesList["studentCodes"][key] = list;
+            }
+            else {
+                console.log(type);
+                codesList["teacherCodes"] = {};
+                codesList["teacherCodes"][key] = list;
+            }
+            docRef.update(codesList);
+        }
         return;
     });
-    return (result.then(() => { console.log("Returned " + valid); return (valid); }));
+    return (result.then(() => {
+        console.log("Returned " + valid);
+        return valid;
+    }));
 });
 //# sourceMappingURL=index.js.map
