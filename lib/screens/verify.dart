c0cb959644f7d00cecdf4ed85404d5dc5082139e
Hallis school <Hallis1221@users.noreply.github.com>
2021-03-01 11:43:19 +0100

    Update verify.dart

--- lib/screens/verify.dart
+++ lib/screens/verify.dart
@@ -2,7 +2,6 @@
  You may not use, distribute and modify this code unless a license is granted. 
  If so use, distribution and modification can be done under the terms of the license.*/
 
-import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_pin_code_fields/flutter_pin_code_fields.dart';
 import 'package:ukeplaner/logic/firebase/auth_services.dart';
