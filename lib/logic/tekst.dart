14bf2d11fbdce9239344f37e27bc62f4fcc38536
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-02 19:35:46 +0100

    Update tekst.dart

--- lib/logic/tekst.dart
+++ lib/logic/tekst.dart
@@ -3,3 +3,16 @@ extension StringExtension on String {
     return "${this[0].toUpperCase()}${this.substring(1)}";
   }
 }
+
+String semesterFormatted(semester) {
+  switch (semester) {
+    case 1:
+      return "Termin en";
+      break;
+    case 2:
+      return "Termin to";
+
+    default:
+      return "Du er ikke i en termin!";
+  }
+}
