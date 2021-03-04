a6e129e2af738e909299daef3a31fe77b79c7a20
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-04 18:09:59 +0100

    Create dayClass.dart

---
+++ lib/logic/dayClass.dart
@@ -0,0 +1,14 @@
+class DayClass {
+  String className;
+  String rom;
+
+  String startTime;
+  String endTime;
+
+  DayClass({
+    this.startTime,
+    this.endTime,
+    this.className,
+    this.rom,
+  });
+}
