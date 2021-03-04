89bf34cbfb74086b3d61cb9d595b9634e182bd8a
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-04 18:10:06 +0100

    Update class.dart

--- lib/logic/class.dart
+++ lib/logic/class.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:ukeplaner/logic/classTimes.dart';
 
-class Class {
+class ClassModel {
   String className;
   String rom;
 
   List<ClassTime> times;
 
-  Class({
-    @required this.times,
-    @required className,
-    @required rom,
+  ClassModel({
+    this.times,
+    this.className,
+    this.rom,
   });
 }
