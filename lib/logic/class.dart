63a3445c852c1558f4e58939533da21ed5916c9a
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-04 19:07:26 +0100

    Update class.dart

--- lib/logic/class.dart
+++ lib/logic/class.dart
@@ -4,10 +4,12 @@ import 'package:ukeplaner/logic/classTimes.dart';
 class ClassModel {
   String className;
   String rom;
+  String teacher;
 
   List<ClassTime> times;
 
   ClassModel({
+    this.teacher,
     this.times,
     this.className,
     this.rom,
