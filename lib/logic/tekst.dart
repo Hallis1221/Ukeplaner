94e9943ca034ee0da57341380705de298ae41d4b
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-04 19:07:34 +0100

    Update tekst.dart

--- lib/logic/tekst.dart
+++ lib/logic/tekst.dart
@@ -16,3 +16,46 @@ String semesterFormatted(semester) {
       return "Du er ikke i en termin!";
   }
 }
+
+String convertDoubleToTime(double _double) {
+  String _string = _double.toString();
+  String beforeResult = "";
+  String afterResult = "";
+  List charactersBeforeSeperator = [];
+  List charactersAfterSeperator = [];
+  bool beforeSeperator = true;
+  for (var i = 0; i < _string.length; i++) {
+    try {
+      int.parse(_string[i]);
+    } catch (e) {
+      beforeSeperator = false;
+      continue;
+    }
+    if (beforeSeperator) {
+      charactersBeforeSeperator.add(_string[i]);
+    } else if (!beforeSeperator) {
+      charactersAfterSeperator.add(_string[i]);
+    }
+  }
+  if (charactersBeforeSeperator.length <= 1) {
+    for (var character in charactersBeforeSeperator) {
+      beforeResult = beforeResult + "0" + character;
+    }
+  } else {
+    for (var character in charactersBeforeSeperator) {
+      beforeResult = beforeResult + character;
+    }
+  }
+  if (charactersAfterSeperator.length <= 1) {
+    for (var character in charactersAfterSeperator) {
+      afterResult = afterResult + character + "0";
+    }
+  } else {
+    for (var character in charactersAfterSeperator) {
+      afterResult = afterResult + character;
+    }
+  }
+
+  String result = "$beforeResult:$afterResult";
+  return result;
+}
