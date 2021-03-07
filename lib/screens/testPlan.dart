c3d0b6a039af636e7de4cad53c0a30b42dd01914
Maazteren <Maaz-kgpc@users.noreply.github.com>
2021-03-07 13:40:09 +0100

    lagde ny side for prøveplanen

---
+++ lib/screens/testPlan.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Testplan extends StatelessWidget {
+  const Testplan({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
