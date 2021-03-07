885afc12f0cd6a065d3e8888b0d45a2d41a2e60d
Maazteren <Maaz-kgpc@users.noreply.github.com>
2021-03-07 15:01:21 +0100

    Create weekPlan.dart

---
+++ lib/screens/weekPlan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WeekPlan extends StatelessWidget {
+  const WeekPlan({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text(
+        'Maaz er et geni',
+        style: TextStyle(fontSize: 50),
+      ),
+    );
+  }
+}
