62b7ecb15a9ebb16872e5eae1003ac1b75861b52
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-09 17:39:50 +0100

    Update weekPlan.dart
    Co-Authored-By: Maazteren <72601521+Maazteren@users.noreply.github.com>

--- lib/screens/weekPlan.dart
+++ lib/screens/weekPlan.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:ukeplaner/config/config.dart';
 import 'package:ukeplaner/screens/login.dart';
+import 'home.dart';
+import 'package:week_of_year/week_of_year.dart';
+import '../logic/tekst.dart';
 
 class WeekPlan extends StatelessWidget {
-  const WeekPlan({Key key}) : super(key: key);
+  const WeekPlan({
+    Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,17 +26,121 @@ class WeekPlan extends StatelessWidget {
           Expanded(
               child: Container(
             color: Colors.green,
-            child: Row(children: [
-              Column(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Container(
-                    child: Text('max kan ikke matte'),
+                  WeekPlanColumn(
+                    weekplanIndex: 1,
+                    week: 10,
                   ),
-                  WeekPlanBox()
-                ],
-              ),
-            ]),
+                ]),
           ))
+        ],
+      ),
+    );
+  }
+}
+
+class WeekPlanColumn extends StatelessWidget {
+  const WeekPlanColumn(
+      {Key key, @required this.weekplanIndex, @required this.week})
+      : super(key: key);
+
+  final int weekplanIndex;
+  final int week;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width / 5,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            child: WeekPlanerTitle(week: week, weekplanIndex: weekplanIndex),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          WeekPlanBox()
+        ],
+      ),
+    );
+  }
+}
+
+class WeekPlanerTitle extends StatefulWidget {
+  const WeekPlanerTitle({
+    Key key,
+    @required this.week,
+    @required this.weekplanIndex,
+  }) : super(key: key);
+
+  final int week;
+  final int weekplanIndex;
+
+  @override
+  _WeekPlanerTitleState createState() => _WeekPlanerTitleState();
+}
+
+class _WeekPlanerTitleState extends State<WeekPlanerTitle> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        DateTime date = DateTime(now.year, 1, 1);
+
+        date =
+            findFirstDateOfTheWeek(date.add(Duration(days: (7 * widget.week))))
+                .add(Duration(days: widget.weekplanIndex - 1));
+
+        now = DateTime(
+          getDate()["dateTime"].year,
+          getDate()["dateTime"].month,
+          getDate()["dateTime"].day,
+        );
+        Duration difference = date.difference(now);
+        print(difference);
+        if (difference.inDays < 0) {
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text("Du kan bare se datoer i framtiden")));
+          return;
+        }
+
+        currentPageSelected = difference.inDays;
+
+        Navigator.of(context).pushNamed("/dayplan");
+        print("cps: $currentPageSelected");
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              height: 15,
+              width: 15,
+              color: Colors.transparent,
+              child: VerticalDivider(
+                thickness: 1.2,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Text(getWeekIndexName(widget.weekplanIndex).capitalize()),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              height: 15,
+              width: 15,
+              color: Colors.transparent,
+              child: VerticalDivider(
+                thickness: 1.2,
+                color: Colors.black,
+              ),
+            ),
+          )
         ],
       ),
     );
