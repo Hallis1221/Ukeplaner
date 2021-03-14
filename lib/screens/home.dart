8a6abe5bed1a5b7d93a438342ec482f772fa7abe
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-14 15:50:58 +0100

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:ukeplaner/config/config.dart';
 import 'package:ukeplaner/icons/custom_icons.dart';
+import 'dayPlan.dart';
 import 'package:ukeplaner/logic/class.dart';
 import 'package:ukeplaner/logic/classTimes.dart';
 import 'package:ukeplaner/logic/dayClass.dart';
@@ -156,66 +157,85 @@ Future<List<Widget>> getLekserWidgets(context, subjects, date) async {
       for (Lekse lekse in completeDayClass.lekser) {
         children.add(Padding(
           padding: const EdgeInsets.only(left: 7.5, right: 7.5, bottom: 25),
-          child: Container(
-            width: MediaQuery.of(context).size.width / 2.2,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 20,
-                ),
-                Icon(
-                  (() {
-                    if (iconIndex[lekse.tittel] != null) {
-                      return iconIndex[lekse.tittel];
-                    } else
-                      return iconIndex["default"];
-                  }()),
-                  color: Colors.white,
-                  size: 90,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  lekse.fag,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 25,
+          child: GestureDetector(
+            onTap: () => showLekse(
+              context,
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(
+                    height: 15,
                   ),
-                ),
-                Text(
-                  "${DateFormat(DateFormat.WEEKDAY).format(lekse.date).capitalize()} uke ${lekse.date.weekOfYear}",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 15,
+                  Center(
+                    child: ListTile(
+                      title: Text(lekse.tittel),
+                      subtitle: Text(lekse.beskrivelse),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-              ],
+                ],
+              ),
             ),
-            decoration: BoxDecoration(
-              color: (() {
-                Random rnd = new Random();
-                int min = 0, max = lekserColors.length;
-                int r = min + rnd.nextInt(max - min);
-                int maxColorsLen = brukteFarger.length;
+            child: Container(
+              width: MediaQuery.of(context).size.width / 2.2,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Icon(
+                    (() {
+                      if (iconIndex[lekse.tittel] != null) {
+                        return iconIndex[lekse.tittel];
+                      } else
+                        return iconIndex["default"];
+                    }()),
+                    color: Colors.white,
+                    size: 90,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    lekse.fag,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
+                  ),
+                  Text(
+                    "${DateFormat(DateFormat.WEEKDAY).format(lekse.date).capitalize()} uke ${lekse.date.weekOfYear}",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                color: (() {
+                  Random rnd = new Random();
+                  int min = 0, max = lekserColors.length;
+                  int r = min + rnd.nextInt(max - min);
+                  int maxColorsLen = brukteFarger.length;
 
-                if (maxColorsLen <= max) {
-                  while (brukteFarger.contains(r)) {
-                    r = min + rnd.nextInt(max - min);
+                  if (maxColorsLen <= max) {
+                    while (brukteFarger.contains(r)) {
+                      r = min + rnd.nextInt(max - min);
+                    }
+                    print("brukte: $brukteFarger");
+                    brukteFarger.add(r);
                   }
-                  print("brukte: $brukteFarger");
-                  brukteFarger.add(r);
-                }
 
-                return lekserColors[r];
-              }()),
-              borderRadius: BorderRadius.circular(
-                35,
+                  return lekserColors[r];
+                }()),
+                borderRadius: BorderRadius.circular(
+                  35,
+                ),
               ),
             ),
           ),
