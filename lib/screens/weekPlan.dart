74ba0898d0f76beb40d952818910ab85afd03ddc
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-18 14:58:47 +0100

    Update weekPlan.dart

--- lib/screens/weekPlan.dart
+++ lib/screens/weekPlan.dart
@@ -297,20 +297,6 @@ class _WeekPlanerTitleState extends State<WeekPlanerTitle> {
   }
 }
 
-Duration getWeekDateDifference(weekplanIndex, week) {
-  DateTime date = DateTime(now.year, 1, 1);
-
-  date = findFirstDateOfTheWeek(date.add(Duration(days: (7 * week))))
-      .add(Duration(days: weekplanIndex - 1));
-  now = DateTime(
-    getDate()["dateTime"].year,
-    getDate()["dateTime"].month,
-    getDate()["dateTime"].day,
-  );
-  Duration difference = date.difference(now);
-  return difference;
-}
-
 class WeekPlanBox extends StatelessWidget {
   const WeekPlanBox({
     Key key,
