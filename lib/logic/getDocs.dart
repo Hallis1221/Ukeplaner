f5b2c9780b7aeb5e07d5b139cf1452157752f2ca
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-18 15:14:17 +0100

    Update getDocs.dart

--- lib/logic/getDocs.dart
+++ lib/logic/getDocs.dart
@@ -1,3 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firestore_cache/firestore_cache.dart';
+import 'package:ukeplaner/config/config.dart';
+
 Future<QuerySnapshot> getDocs() async {
   final DocumentReference cacheDocRef = db.doc('status/status');
   final String cacheField = 'updatedAt';
