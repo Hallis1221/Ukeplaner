2640598070a4973f1d8d8f6303d41921772f35b0
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-18 15:14:10 +0100

    Update startTime.dart

--- lib/logic/startTime.dart
+++ lib/logic/startTime.dart
@@ -1,3 +1,9 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:native_shared_preferences/native_shared_preferences.dart';
+import 'package:ukeplaner/config/config.dart';
+
 startTime(context) async {
   NativeSharedPreferences prefs = await NativeSharedPreferences.getInstance();
   bool firstTime = prefs.getBool('first_time');
