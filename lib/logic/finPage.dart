e0380de66d579ecdda040fdb1921d9799834da48
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-18 14:58:45 +0100

    Update finPage.dart

---
+++ lib/logic/finPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FindPage extends StatelessWidget {
+  const FindPage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    startTime(context);
+    return Loading();
+  }
+}
