ec6f92dd00ca16648994c2bcc58812721f6a44c8
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-18 15:14:32 +0100

    Update finPage.dart

--- lib/logic/finPage.dart
+++ lib/logic/finPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:loading/loading.dart';
+import 'package:ukeplaner/logic/startTime.dart';
 
 class FindPage extends StatelessWidget {
   const FindPage({
