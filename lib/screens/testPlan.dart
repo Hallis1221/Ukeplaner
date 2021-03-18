3094407216a4539d847f820afb6324905c47fc8b
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-18 14:58:52 +0100

    Update testPlan.dart

--- lib/screens/testPlan.dart
+++ lib/screens/testPlan.dart
@@ -91,25 +91,3 @@ class Testplan extends StatelessWidget {
     );
   }
 }
-
-Future<List<Test>> getTests() async {
-  List<Test> tests = [];
-  for (String code in config.cloudClassesCodes) {
-    var currentTest;
-    DocumentReference documentReference =
-        config.db.collection("classes").doc(code);
-    config.analytics.logEvent(name: "get_classes_$code");
-    print("got_$code");
-    await FirestoreCache.getDocument(documentReference).then((value) {
-      currentTest = value.data()["tests"];
-      for (var test in currentTest) {
-        Timestamp timeStamp = test["date"];
-        tests.add(new Test(
-            date: timeStamp.toDate(),
-            message: test["message"],
-            title: test["title"]));
-      }
-    });
-  }
-  return tests;
-}
