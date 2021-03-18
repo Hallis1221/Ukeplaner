05a863e86c26db40bcb1fd19ada0542bc6cff951
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-18 14:58:55 +0100

    Update verify.dart

--- lib/screens/verify.dart
+++ lib/screens/verify.dart
@@ -146,35 +146,6 @@ class InputCard extends StatelessWidget {
   }
 }
 
-class BottomCircle extends StatelessWidget {
-  const BottomCircle({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        color: Theme.of(context).primaryColor,
-        boxShadow: <BoxShadow>[
-          BoxShadow(
-            blurRadius: 10,
-            color: Theme.of(context).shadowColor,
-          )
-        ],
-        borderRadius: BorderRadius.vertical(
-          top: Radius.elliptical(
-            MediaQuery.of(context).size.width * 3,
-            250,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 class CodeInputter extends StatefulWidget {
   const CodeInputter({Key key}) : super(key: key);
 
