cb289fb9d765cac026eef43ecc2087317fb66fcb
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-18 15:14:21 +0100

    Update getTests.dart

--- lib/logic/getTests.dart
+++ lib/logic/getTests.dart
@@ -1,10 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ukeplaner/config/config.dart';
+import 'package:firestore_cache/firestore_cache.dart';
+import 'package:ukeplaner/logic/test.dart';
+
 Future<List<Test>> getTests() async {
   List<Test> tests = [];
-  for (String code in config.cloudClassesCodes) {
+  for (String code in cloudClassesCodes) {
     var currentTest;
-    DocumentReference documentReference =
-        config.db.collection("classes").doc(code);
-    config.analytics.logEvent(name: "get_classes_$code");
+    DocumentReference documentReference = db.collection("classes").doc(code);
+    analytics.logEvent(name: "get_classes_$code");
     print("got_$code");
     await FirestoreCache.getDocument(documentReference).then((value) {
       currentTest = value.data()["tests"];
