b7a93f8d5a1e3220de8ec1385e075b9d53d6b9a7
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-25 21:33:16 +0100

    Update getLekserWidgets.dart
    Sikre at samme lekse ikke blir vist flere ganger

--- lib/logic/getLekserWidgets.dart
+++ lib/logic/getLekserWidgets.dart
@@ -10,6 +10,8 @@ import 'leske.dart';
 import 'package:ukeplaner/config/config.dart';
 import 'makeCompleteDayClass.dart';
 
+List<String> rowChildrenController = [];
+
 Future<List<Widget>> getLekserWidgets(context, subjects, date) async {
   List<Widget> children = [];
   List brukteFarger = [];
@@ -17,91 +19,96 @@ Future<List<Widget>> getLekserWidgets(context, subjects, date) async {
       .then((value) {
     for (CompleteDayClass completeDayClass in value) {
       for (Lekse lekse in completeDayClass.lekser) {
-        children.add(Padding(
-          padding: const EdgeInsets.only(left: 7.5, right: 7.5, bottom: 25),
-          child: GestureDetector(
-            onTap: () => showLekse(
-              context,
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Center(
-                    child: ListTile(
-                      title: Text(lekse.tittel),
-                      subtitle: Text(lekse.beskrivelse),
+        String uid =
+            "${lekse.tittel}.${lekse.fag}.${lekse.date}.${lekse.beskrivelse}";
+        if (!rowChildrenController.contains(uid)) {
+          rowChildrenController.add(uid);
+          children.add(Padding(
+            padding: const EdgeInsets.only(left: 7.5, right: 7.5, bottom: 25),
+            child: GestureDetector(
+              onTap: () => showLekse(
+                context,
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(
+                      height: 15,
                     ),
-                  ),
-                ],
+                    Center(
+                      child: ListTile(
+                        title: Text(lekse.tittel),
+                        subtitle: Text(lekse.beskrivelse),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            child: Container(
-              width: MediaQuery.of(context).size.width / 2.2,
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Icon(
-                    (() {
-                      if (iconIndex[lekse.tittel] != null) {
-                        return iconIndex[lekse.tittel];
-                      } else
-                        return iconIndex["default"];
-                    }()),
-                    color: Colors.white,
-                    size: 90,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    lekse.fag,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 25,
+              child: Container(
+                width: MediaQuery.of(context).size.width / 2.2,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 20,
                     ),
-                  ),
-                  Text(
-                    "${DateFormat(DateFormat.WEEKDAY).format(lekse.date).capitalize()} uke ${lekse.date.weekOfYear}",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                    Icon(
+                      (() {
+                        if (iconIndex[lekse.tittel] != null) {
+                          return iconIndex[lekse.tittel];
+                        } else
+                          return iconIndex["default"];
+                      }()),
                       color: Colors.white,
-                      fontSize: 15,
+                      size: 90,
                     ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                ],
-              ),
-              decoration: BoxDecoration(
-                color: (() {
-                  Random rnd = new Random();
-                  int min = 0, max = lekserColors.length;
-                  int r = min + rnd.nextInt(max - min);
-                  int maxColorsLen = brukteFarger.length;
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      lekse.fag,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                    Text(
+                      "${DateFormat(DateFormat.WEEKDAY).format(lekse.date).capitalize()} uke ${lekse.date.weekOfYear}",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+                decoration: BoxDecoration(
+                  color: (() {
+                    Random rnd = new Random();
+                    int min = 0, max = lekserColors.length;
+                    int r = min + rnd.nextInt(max - min);
+                    int maxColorsLen = brukteFarger.length;
 
-                  if (maxColorsLen <= max) {
-                    while (brukteFarger.contains(r)) {
-                      r = min + rnd.nextInt(max - min);
+                    if (maxColorsLen <= max) {
+                      while (brukteFarger.contains(r)) {
+                        r = min + rnd.nextInt(max - min);
+                      }
+
+                      brukteFarger.add(r);
                     }
 
-                    brukteFarger.add(r);
-                  }
-
-                  return lekserColors[r];
-                }()),
-                borderRadius: BorderRadius.circular(
-                  35,
+                    return lekserColors[r];
+                  }()),
+                  borderRadius: BorderRadius.circular(
+                    35,
+                  ),
                 ),
               ),
             ),
-          ),
-        ));
+          ));
+        }
       }
     }
   });
