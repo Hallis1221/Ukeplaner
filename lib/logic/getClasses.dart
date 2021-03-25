6abb1568074587d5bb31e31c077d73f258b83346
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-25 20:54:51 +0100

    Update getClasses.dart
    Remove print

--- lib/logic/getClasses.dart
+++ lib/logic/getClasses.dart
@@ -2,5 +2,6 @@ import 'package:ukeplaner/config/config.dart';
 import 'package:ukeplaner/logic/class.dart';
 
 Future<List<ClassModel>> getClasses() async {
+  print("classes: $classes");
   return classes;
 }
