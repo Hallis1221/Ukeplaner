5404152d4a1d73de348e6a6c260d9196982187b3
Hallis - home main <halvorvivelstad@gmail.com>
2021-03-28 19:39:20 +0200

    Update getWeekDateDIfference.dart

--- lib/logic/getWeekDateDIfference.dart
+++ lib/logic/getWeekDateDIfference.dart
@@ -2,17 +2,18 @@ import 'package:ukeplaner/logic/dates.dart';
 
 Duration getWeekDateDifference(weekplanIndex, week) {
   DateTime now = DateTime.now();
+  DateTime nowDate = now;
   DateTime date = DateTime(now.year, 1, 1);
 
   date = findFirstDateOfTheWeek(date.add(Duration(days: (7 * week))))
       .add(Duration(days: weekplanIndex - 1));
-  now = DateTime(
+  nowDate = DateTime(
     getDate()["dateTime"].year,
     getDate()["dateTime"].month,
     getDate()["dateTime"].day,
   );
 
   date = skipWeekend(date)["dateTime"];
-  Duration difference = date.difference(now);
+  Duration difference = date.difference(nowDate);
   return difference;
 }
