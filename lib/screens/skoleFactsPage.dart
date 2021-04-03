73b8d123b0288e124f668ca7c62d28539dc30e05
Maazteren <Maaz-kgpc@users.noreply.github.com>
2021-04-03 17:04:52 +0200

    Create skoleFactsPage.dart

---
+++ lib/screens/skoleFactsPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SkoleFacts extends StatelessWidget {
+  const SkoleFacts({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
