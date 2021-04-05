d50471d5870c3bc11e4890ded91d923304cae0c0
Hallis - home main <halvorvivelstad@gmail.com>
2021-04-05 11:24:33 +0200

    Update settings.dart

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ukeplaner/logic/oss_license.dart';
+import 'package:ukeplaner/main.dart';
 
 class SettingsMenu extends StatelessWidget {
   const SettingsMenu({Key key}) : super(key: key);
@@ -21,6 +22,24 @@ class SettingsMenu extends StatelessWidget {
                 ),
                 onTap: () => Navigator.of(context).pushNamed('/licenses'),
               ),
+              ListTile(
+                leading: Icon(Icons.color_lens),
+                title: Text(prefs.getString('color_preset') +
+                    " farge palet. Trykk for å bytte."),
+                onTap: () {
+                  parent.setState(() {
+                    switch (prefs.getString('color_preset')) {
+                      case "halvor":
+                        prefs.setString('color_preset', "maaz");
+                        break;
+                      case "maaz":
+                        prefs.setString('color_preset', "halvor");
+                        break;
+                      default:
+                    }
+                  });
+                },
+              )
             ],
           ),
         ],
