b9afe6d08908617eee0fa44806bae38a8aac604d
Hallis - home main <halvorvivelstad@gmail.com>
2021-04-05 11:18:15 +0200

    Update config.dart

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -77,10 +77,44 @@ List<Color> lekserColors = [
   Color.fromARGB(255, 101, 135, 227),
   Color.fromARGB(255, 0, 50, 155),
 ];
-ThemeData theme = ThemeData(
+ThemeData maazTheme = ThemeData(
   // start color
   backgroundColor: Color.fromARGB(255, 254, 247, 229),
   scaffoldBackgroundColor: Color.fromARGB(255, 254, 247, 229),
+  primaryColor: Color.fromARGB(255, 79, 68, 255),
+  accentColor: Color.fromARGB(255, 48, 147, 152),
+  errorColor: Color.fromARGB(255, 229, 25, 25),
+  buttonColor: Color.fromARGB(255, 79, 58, 255),
+  shadowColor: Color.fromARGB(255, 126, 180, 238),
+  splashColor: Color.fromARGB(255, 24, 20, 93),
+  indicatorColor: Color.fromARGB(255, 24, 20, 93),
+  hintColor: Color.fromARGB(105, 158, 158, 158),
+  // end color
+  iconTheme: IconThemeData(opacity: 255),
+  appBarTheme: AppBarTheme(
+    color: Color.fromARGB(255, 79, 58, 255),
+    elevation: 0,
+    centerTitle: true,
+  ),
+  fontFamily: "NirmalaUI",
+  inputDecorationTheme: InputDecorationTheme(
+    filled: false,
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(
+        Radius.circular(
+          10,
+        ),
+      ),
+      borderSide: BorderSide(
+        color: Color.fromARGB(255, 79, 68, 255),
+      ),
+    ),
+  ),
+);
+ThemeData halvorTheme = ThemeData(
+  // start color
+  backgroundColor: Color.fromARGB(255, 244, 243, 249),
+  scaffoldBackgroundColor: Color.fromARGB(255, 244, 243, 249),
   primaryColor: Color.fromARGB(255, 79, 68, 255),
   accentColor: Color.fromARGB(255, 48, 147, 152),
   errorColor: Color.fromARGB(255, 229, 25, 25),
