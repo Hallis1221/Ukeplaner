1120bc9ac9e0306b4ed9299bcdc18ea3887086dd
Hallis - home main <halvorvivelstad@gmail.com>
2021-04-05 10:43:59 +0200

    Update firebase.dart

--- lib/logic/firebase/firebase.dart
+++ lib/logic/firebase/firebase.dart
@@ -81,29 +81,31 @@ class LocalFirebaseApp extends StatelessWidget {
                     debugShowCheckedModeBanner: false,
                     onGenerateRoute: (settings) {
                       var page = routes[settings.name];
-
                       if (settings.name == "/") {
-                        print("Route was defult");
+                        return PageRouteBuilder(
+                          pageBuilder:
+                              (context, animation, secondaryAnimation) => page,
+                        );
                       } else {
                         print("route name: ${settings.name}");
-                      }
-                      return PageRouteBuilder(
-                        pageBuilder: (context, animation, secondaryAnimation) =>
-                            page,
-                        transitionsBuilder:
-                            (context, animation, secondaryAnimation, child) {
-                          var begin = Offset(1.0, 0.0);
-                          var end = Offset.zero;
-                          var curve = Curves.ease;
+                        return PageRouteBuilder(
+                          pageBuilder:
+                              (context, animation, secondaryAnimation) => page,
+                          transitionsBuilder:
+                              (context, animation, secondaryAnimation, child) {
+                            var begin = Offset(1.0, 0.0);
+                            var end = Offset.zero;
+                            var curve = Curves.ease;
 
-                          var tween = Tween(begin: begin, end: end)
-                              .chain(CurveTween(curve: curve));
-                          return SlideTransition(
-                            position: animation.drive(tween),
-                            child: child,
-                          );
-                        },
-                      );
+                            var tween = Tween(begin: begin, end: end)
+                                .chain(CurveTween(curve: curve));
+                            return SlideTransition(
+                              position: animation.drive(tween),
+                              child: child,
+                            );
+                          },
+                        );
+                      }
                     },
                     navigatorObservers: [
                       FirebaseAnalyticsObserver(
