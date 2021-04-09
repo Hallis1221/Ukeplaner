719078d2b7a2d306ff40a6ee31b69e02525da674
Maazteren <maamek@kg.vgs.no>
2021-04-09 14:01:25 +0200

    Update getTests.dart

--- lib/logic/getTests.dart
+++ lib/logic/getTests.dart
@@ -4,25 +4,37 @@ import '../logic/firebase/firestore.dart';
 import '../logic/test.dart';
 
 Future<List<Test>> getTests() async {
-  List<Test> tests = [];
-  for (String code in cloudClassesCodes) {
-    var currentTest;
+  List<Test> tests = await _getList();
+  print("4");
+  return tests;
+}
+
+
+Future<List<Test>> _getList() async {
+  List tests = [];
+   for (String code in cloudClassesCodes) {
+    print(2);
     DocumentReference documentReference = db.collection("classes").doc(code);
 
-    await getDocument(
-            documentReference: documentReference,
-            timeTrigger: new Duration(hours: 1))
-        .then((value) {
-      currentTest = value["tests"];
-      for (var test in currentTest) {
-        Timestamp timeStamp = test["date"];
-        tests.add(new Test(
-            date: timeStamp.toDate(),
-            message: test["message"],
-            title: test["title"]));
-      }
-    });
-  }
+    var value = await getDocument(
+        documentReference: documentReference,
+        timeTrigger: new Duration(hours: 1));
 
+    print("VERID ER $value");
+    List currentTest = value["tests"];
+    print("tests;  $currentTest");
+    for (var test in currentTest) {
+      print("test; $test");
+      int timeStamp = test["TIMESTAMP_date"];
+      tests.add(new Test(
+          date: Timestamp.fromMicrosecondsSinceEpoch(timeStamp).toDate(),
+          message: test["message"],
+          title: test["title"]));
+      print("tests list: $tests");
+    }
+    print(3);
+    print("KOM HIT");
+  }
+  print("KOMER HIT");
   return tests;
 }
