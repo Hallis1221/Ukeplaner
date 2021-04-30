f1f8bf36451c3b01cf2ed135a3860e3f976c3d04
Maazteren <maamek@kg.vgs.no>
2021-04-30 14:26:57 +0200

    Update skoleFactsPage.dart

--- lib/screens/skoleFactsPage.dart
+++ lib/screens/skoleFactsPage.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:ukeplaner/logic/facts.dart';
 
 class SkoleFacts extends StatelessWidget {
   const SkoleFacts({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    List<Fact> fakta = <Fact>[];
     return Scaffold(
       appBar: AppBar(
         title: Text("Skole Facts"),
