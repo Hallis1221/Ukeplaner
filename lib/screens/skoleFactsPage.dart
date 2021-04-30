456648d538c07d3f9c6b97b7e7b390af97d8a5b1
Maazteren <maamek@kg.vgs.no>
2021-04-30 14:12:31 +0200

    Update skoleFactsPage.dart

--- lib/screens/skoleFactsPage.dart
+++ lib/screens/skoleFactsPage.dart
@@ -5,6 +5,10 @@ class SkoleFacts extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Skole Facts"),
+      ),
+    );
   }
 }
