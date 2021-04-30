6d004646954ceb93cf5f61360197c57283319b5f
Maazteren <maamek@kg.vgs.no>
2021-04-30 14:30:07 +0200

    Update skoleFactsPage.dart

--- lib/screens/skoleFactsPage.dart
+++ lib/screens/skoleFactsPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:ukeplaner/logic/facts.dart';
 
 class SkoleFacts extends StatelessWidget {
@@ -6,7 +7,16 @@ class SkoleFacts extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Fact> fakta = <Fact>[];
+    List<Fact> fakta = <Fact>[
+      new Fact(
+        question: 'text',
+        answear: 'text',
+        animation: LottieBuilder.asset(
+          'assets/animations/UtvAnimasjon.json',
+          height: 125,
+        ),
+      )
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Text("Skole Facts"),
