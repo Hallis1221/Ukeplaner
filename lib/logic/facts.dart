efd447e14d72a4f4abf7d016d2ade35b1ea89829
Maazteren <maamek@kg.vgs.no>
2021-04-30 14:22:40 +0200

    Create facts.dart

---
+++ lib/logic/facts.dart
@@ -0,0 +1,13 @@
+import 'package:lottie/lottie.dart';
+
+class Fact {
+  String question;
+  String answear;
+  LottieBuilder animation;
+
+  Fact({
+    this.question,
+    this.answear,
+    this.animation,
+  });
+}
