fc0835223fde769c1cd1b15bc4e700e4eb87ff24
Maazteren <maamek@kg.vgs.no>
2021-04-30 16:34:28 +0200

    Update skoleFactsPage.dart

--- lib/screens/skoleFactsPage.dart
+++ lib/screens/skoleFactsPage.dart
@@ -59,9 +59,9 @@ Route _createRoute(Fact fact) {
       fakta: fact,
     ),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      var begin = Offset(1.0, 0.0);
+      var begin = Offset(1.0, -1.0);
       var end = Offset.zero;
-      var curve = Curves.ease;
+      var curve = Curves.easeOutCubic;
 
       var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
       return SlideTransition(
